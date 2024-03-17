615409e041a14e0dd89ac243026ba9e9fc2d7f8b
alfina zahra <alfinaz916@gmail.com>
2024-03-17 11:57:56 +0700

    feat: add update app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,17 @@
 import { useState } from 'react'
 import './App.css'
+import NavBar from './components/NavBar'
+import Main from './components/Main'
+import Footer from './components/Footer'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
-      
+      <NavBar/>
+      <Main/>
+      <Footer/>
     </>
   )
 }
