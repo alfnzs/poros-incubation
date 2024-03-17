646311b849d679af8901545b0126dfe02cfae5b2
alfina zahra <alfinaz916@gmail.com>
2024-03-17 11:52:07 +0700

    feat: add jsx navbar component

---
+++ src/components/NavBar/index.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+import "./NavBar.module.css";
+
+function NavBar() {
+  return (
+    <nav className={"style.NavBar"}>
+      <div>
+          <img src="https://porosfilkom.ub.ac.id/_next/static/media/logo-poros.cd896a0d.svg" alt="logo-poros" />
+        </div>
+        <div>
+          <h3>Incubation <br />Week 1</h3>
+        </div>
+    </nav>
+  );
+}
+
+export default NavBar;
