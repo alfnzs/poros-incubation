376d3714e071027970090f69a2712b8ceca1bb06
alfina zahra <alfinaz916@gmail.com>
2024-03-17 11:55:55 +0700

    feat: add jsx footer component

---
+++ src/components/Footer/index.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import "./Footer.module.css"
+
+function Footer() {
+  return (
+    <footer>
+      <h4>Made by Alfina</h4>
+    </footer>
+  );
+}
+
+export default Footer;
